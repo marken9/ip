9fe182e2227525f9b40046578dffeb9cc76e91d6
marken9 <177712014+marken9@users.noreply.github.com>
2026-01-24 22:47:42 +0800

    Add Level-0

---
+++ src/main/java/Painter.java
@@ -0,0 +1,14 @@
+public class Painter {
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printLine();
+        System.out.println("Hello! I'm Painter\nWhat can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+
+    }
+}
