34cf077973f359092c4650df388433c2a012e8c5
marken9 <177712014+marken9@users.noreply.github.com>
2026-01-28 22:19:55 +0800

    Add simple todo list for level 1 to 3 and complete A-CodingStandard

--- src/main/java/Painter.java
+++ src/main/java/Painter.java
@@ -1,14 +1,75 @@
+import java.util.Scanner;
+
+import task.Task;
+
 public class Painter {
     public static void printLine() {
         System.out.println("____________________________________________________________");
     }
 
-    public static void main(String[] args) {
+    public static void printSentence(String x) {
         printLine();
-        System.out.println("Hello! I'm Painter\nWhat can I do for you?");
+        System.out.println(x);
         printLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printLine();
+    }
 
+    public static void markTaskList(boolean mark, Task[] taskList, int nTasks, int n) {
+        if (n > nTasks) {
+            printSentence("You only have " + nTasks + " tasks but you entered " + n + ".");
+            return;
+        }
+        if (mark) {
+            taskList[n - 1].markAsDone();
+            printSentence("Nice! I've marked this task as done: [X] " + taskList[n - 1].getDescription());
+        } else {
+            taskList[n - 1].markAsUndone();
+            printSentence("OK, I've marked this task as not done yet: [ ] " + taskList[n - 1].getDescription());
+        }
+    }
+
+    public static void printTaskList(Task[] taskList, int nTasks) {
+        printLine();
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < nTasks; i += 1) {
+            System.out.println((i + 1) + ".[" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
+        }
+        printLine();
+    }
+
+
+    public static void main(String[] args) {
+        printSentence("Hello! I'm Painter\nWhat can I do for you?");
+
+        Task[] taskList = new Task[100];
+        int nTasks = 0;
+
+        while (true) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            line = line.strip();
+
+            switch (line) {
+            case "bye":
+                printSentence("Bye. Hope to see you again soon!");
+                return;
+            case "list":
+                printTaskList(taskList, nTasks);
+                break;
+            default:
+                String[] words = line.split(" ");
+                // Code assumes that mark/unmark is called correctly
+                // e.g. "mark string" will crash.
+                if (words[0].equals("mark")) {
+                    markTaskList(true, taskList, nTasks, Integer.parseInt(words[1]));
+                } else if (words[0].equals("unmark")) {
+                    markTaskList(false, taskList, nTasks, Integer.parseInt(words[1]));
+                } else {
+                    taskList[nTasks] = new Task(line);
+                    nTasks += 1;
+                    printSentence("Added: " + line);
+                }
+            }
+        }
     }
 }
